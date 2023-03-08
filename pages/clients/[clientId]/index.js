0bed9a35e1f587ae22acb1748b390df135b3e57e
gabin <lestratgabin@gmail.com>
2023-03-08 11:06:15 +0100

    dynamaic nav

--- pages/clients/[clientId]/index.js
+++ pages/clients/[clientId]/index.js
@@ -6,9 +6,21 @@ function ClientsProjectPage() {
 
     console.log(router.query)
 
+    function loadProjectHandler(){
+        // laod data...
+        // then navigate away
+        // router.push(`/clients/${router.query.clientId}/projecta`)
+        // l'on peux aussi ajouter un object
+        router.push({
+            pathname: '/clients/[clientId]/[clientProjectId]',
+            query: {clientId: router.query.clientId, clientProjectId: 'projecta'}
+        })
+    } 
+
     return (
         <div>
             <h1>The projects of a given client</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     )
 }
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,26 @@
+import Link from "next/link"
+
 function ClientsPage() {
+
+    const clients = [
+        {id: 'max', name: 'Maximilian'},
+        {id: 'manu', name: 'Manuel'},
+        {id: 'seb', name: 'sebastian'}
+    ]
+
     return (
         <div>
             <h1>Clients Page</h1>
+            <ul>
+                {clients.map(client => (
+                    <li key={client.id}>
+                        <Link href={{
+                            pathname: '/clients/[clientId]',
+                            query: {clientId: client.id}
+                        }}>{client.name}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
